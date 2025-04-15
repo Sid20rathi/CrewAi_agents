2e6d8ac13166fe8ea62b01e13c11fcb66ce02085
Siddhant Rathi <sid20rathi@gmail.com>
2025-04-15 19:30:17 +0530

    updated llm

--- crew.py
+++ crew.py
@@ -1,24 +1,21 @@
 from crewai import Crew,Process
 from agents import blog_researcher,blog_writer
 from tasks import research_task,write_task
-from crewai import LLM
 import os
 from dotenv import load_dotenv
+from langchain_groq import ChatGroq
 load_dotenv()
 Groqapi=os.getenv("GroqApi")
+agent_llm = ChatGroq(model="llama-3.2-90b-text-preview", api_key=Groqapi)
 
 
-llm = LLM(
-    model="groq/llama-3.2-90b-text-preview",
-    temperature=0.7,
-    api=Groqapi
-)
+
 
 crew = Crew(
     agents=[blog_researcher,blog_writer],
     tasks=[research_task,write_task],
     process=Process.sequential,
-    llm = llm,
+    llm = agent_llm,
     memory=True,
     cache=True,
     max_rpm=100,
--- tools.py
+++ tools.py
@@ -1,6 +1,32 @@
 from crewai_tools import YoutubeChannelSearchTool
+import os
+from dotenv import load_dotenv
+load_dotenv()
+Groqapi=os.getenv("GroqApi")
+HuggingFaceApi=os.getenv("HuggingFaceApi")
+
+
+
 
 youtube_channel_tool = YoutubeChannelSearchTool(
-    youtube_channel_handle='@InspiredForImpact'
+    youtube_channel_handle='@InspiredForImpact',
+    config=dict(
+        llm=dict(
+            provider="groq",
+            config=dict(
+                model="llama-3.2-90b-text-preview",
+                api_key=Groqapi,
+              
+            ),
+        ),
+        embedder=dict(
+            provider="huggingface", 
+            config=dict(
+                model="sentence-transformers/all-MiniLM-L6-v2",
+                api_key=HuggingFaceApi,
+              
+            ),
+        ),
+    )
 )
 
